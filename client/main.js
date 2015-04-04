3eab7bcae202ed25c9a0b8850a7a5ee7a4ed72ac
Egor Berezovskiy <egor.neon@gmail.com>
2015-04-04 11:37:05 +0200

    Add Signup Form

--- client/main.js
+++ client/main.js
@@ -21,6 +21,7 @@ var Groupes = require('./groupes/Groupes');
 var SingleGroupe = require('./groupes/SingleGroupe');
 var Auth = require('./auth/Auth');
 var User = require('./user/User');
+var Signup = require('./user/Signup');
 
 
 var App = React.createClass({
@@ -35,6 +36,7 @@ var App = React.createClass({
                         <DropdownButton eventKey={3} title='Options'>
                             <li><Link to="createArticle">Create Article</Link></li>
                             <li><Link to="auth">Auth (Login, Signup, Logout)</Link></li>
+                            <li><Link to="signup">Signup</Link></li>
                             <li><Link to="singleGroupe" params={{slug: 'toto'}}>single groupes</Link></li>
                         </DropdownButton>
                     </Nav>
@@ -59,6 +61,7 @@ var Routes = (
         <Route name="groupes" handler={Groupes} path="/groupes" />
         <Route name="singleGroupe" handler={SingleGroupe} path="/groupes/:slug" />
         <Route name="user" handler={User} path="/user" />
+        <Route name="signup" handler={Signup} path="/signup" />
     </Route>
 );
 
--- client/user/Signup.js
+++ client/user/Signup.js
@@ -1,24 +1,128 @@
-var React = require('react');
+var React = require('react/addons');
+var moment = require('moment');
+var utils = require('../../commun/utils');
+
+var Grid = require('react-bootstrap/lib/Grid');
+var Row = require('react-bootstrap/lib/Row');
+var Col = require('react-bootstrap/lib/Col');
+var Input = require('react-bootstrap/lib/Input');
+var Button = require('react-bootstrap/lib/Button');
 
 var Signup = React.createClass({
+    mixins: [React.addons.LinkedStateMixin],
     render: function() {
         return (
-            <div />
+            <Grid>
+                <Row>
+                    <Col xs={12}>
+                        <h1>Create new user</h1>
+                        <Row>
+                            <Col xs={12} sm={6}>
+                                <Input
+                                    type='text'
+                                    placeholder='First Name'
+                                    label='First Name'
+                                    ref='first_name'
+                                    valueLink={this.linkState('first_name')}
+                                />
+                            </Col>
+                            <Col xs={12} sm={6}>
+                                <Input
+                                    type='text'
+                                    placeholder='Last Name'
+                                    label='Last Name'
+                                    ref='last_name'
+                                    valueLink={this.linkState('last_name')}
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={12} sm={6}>
+                                <Input
+                                    type='email'
+                                    placeholder='Email'
+                                    label='Email'
+                                    ref='email'
+                                    valueLink={this.linkState('email')}
+                                />
+                            </Col>
+                            <Col xs={12} sm={6}>
+                                <Input
+                                    type='date'
+                                    ref='birthday_date'
+                                    label='Birthday Date'
+                                    valueLink={this.linkState('birthday_date')}
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={12} sm={6}>
+                                <Input
+                                    type='text'
+                                    placeholder='Function'
+                                    label='Function'
+                                    ref='function'
+                                    valueLink={this.linkState('function')}
+                                />
+                            </Col>
+                            <Col xs={12} sm={6}>
+                                <Input
+                                    type='date'
+                                    ref='arrival_date'
+                                    label='Arrival Date'
+                                    valueLink={this.linkState('arrival_date')}
+                                />
+                            </Col>
+                        </Row>
+                        <Input
+                            type='text'
+                            placeholder='Adress'
+                            label='Adress'
+                            ref='adress'
+                            valueLink={this.linkState('adress')}
+                        />
+
+                        <Input
+                            type='textarea'
+                            placeholder='Description'
+                            label='Description'
+                            ref='description'
+                            valueLink={this.linkState('description')}
+                        />
+                        <Button bsStyle='success' onClick={this.submit}>Save</Button>
+                    </Col>
+                </Row>
+            </Grid>
         );
-    }
+    },
+    submit: function () {
+        // TODO validate data
+        var newUser = {
+            email: this.state.email,
+            birthday_date: this.state.birthday_date,
+            first_name: this.state.first_name,
+            last_name: this.state.last_name,
+            adress: this.state.adress,
+            function: this.state.function,
+            description: this.state.description,
+            arrival_date: this.state.arrival_date,
+        };
+        //ArticlesActions.createArticle(newArticle);
+        console.log("Sigup", "submit", newUser);
+    },
+    getInitialState: function() {
+        return {
+            email: "",
+            first_name: "",
+            last_name: "",
+            birthday_date: moment().format(utils.mysqlDateFormat),
+            adress: "",
+            role: "",
+            function: "",
+            description: "",
+            arrival_date: moment().format(utils.mysqlDateFormat),
+        };
+    },
 });
 
 module.exports = Signup;
-
-var newUser = {
-    email: req.body.email,
-    role: req.body.role,
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    birthday_date: req.body.birthday_date,
-    adress: req.body.adress,
-    function: req.body.function,
-    description: req.body.description,
-    arrival_date: req.body.arrival_date
-}
-
