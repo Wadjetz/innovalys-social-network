1df8cdd7af9204c70ce9d8a70636cff4df5f4099
Egor Berezovskiy <egor.neon@gmail.com>
2015-04-11 21:07:53 +0200

    Config socket.io

--- client/chat/ChatStore.js
+++ client/chat/ChatStore.js
@@ -1,7 +1,7 @@
 var Reflux = require('reflux');
 var _ = require('lodash');
 
-var socket = io('http://localhost');
+var socket = io(document.location.origin);
 
 var ChatActions = require('./ChatActions');
 var ChatApi = require('./ChatApi');
