63b5881dc067b64bdbdcad89999e9fb5076dd69f
Egor Berezovskiy <egor.neon@gmail.com>
2015-04-24 16:56:11 +0200

    send comments with single article

--- server/news/news-model.js
+++ server/news/news-model.js
@@ -14,9 +14,15 @@ module.exports.findOneBySlug = function (slug, callback) {
     var sql = "SELECT * FROM news WHERE news.slug = ?";
     var data = [slug];
     db.query(sql, data, function (error, results, fields) {
-        //console.log("news model findOneBySlug", "error", error, "results", results);
-        if (error) console.error(error);
-        callback(error, results, fields);
+        console.log("news model findOneBySlug", "error", error, "results", results);
+        if (error) {
+            console.error(error);
+            callback(error, results, fields);
+        } else if (results.length > 0) {
+            callback(error, results[0], fields);
+        } else {
+            callback(error, null, fields);
+        }
     });
 };
 
