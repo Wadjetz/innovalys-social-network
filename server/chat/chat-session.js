540d96c7fd0f947dd12ca86df90d842609f979da
Egor Berezovskiy <ebe@zenexity.com>
2015-07-05 09:22:25 +0200

    Chat : Fix connection and deconnection

--- server/chat/chat-session.js
+++ server/chat/chat-session.js
@@ -4,17 +4,32 @@ var config = require('../config/config');
 module.exports = function(io, cookieParser, sessionStore) {
   io.use(function(socket, next) {
     var request = socket.request;
-    if(!request.headers.cookie) { return next(new Error('No cookie transmitted.')); } // If we want to refuse authentification, we pass an error to the first callback
+    if(!request.headers.cookie) {
+      console.log("! request.headers.cookie");
+      return next(new Error('No cookie transmitted.'));
+    } // If we want to refuse authentification, we pass an error to the first callback
 
     cookieParser(request, {}, function(parseErr) {
-      if(parseErr) { return next(new Error('Error parsing cookies.')); }
+      if(parseErr) {
+        console.log("! request.headers.cookie");
+        return next(new Error('Error parsing cookies.'));
+      }
       var sidCookie = (request.secureCookies && request.secureCookies[config.EXPRESS_SID_KEY]) ||
                       (request.signedCookies && request.signedCookies[config.EXPRESS_SID_KEY]) ||
                       (request.cookies && request.cookies[config.EXPRESS_SID_KEY]);
       sessionStore.load(sidCookie, function(err, session) {
-        if (err) { return next(err); }
-        else if(!session) { return next(new Error('Session cannot be found/loaded')); }
-        else if (session.isLogged !== true) { return next(new Error('User not logged in')); }
+        if (err) {
+          console.log("sessionStore.load", err);
+          return next(err);
+        }
+        else if(!session) {
+          console.log("Session cannot be found/loaded");
+          return next(new Error('Session cannot be found/loaded'));
+        }
+        else if (session.isLogged !== true) {
+          console.log("User not logged in");
+          return next(new Error('User not logged in'));
+        }
         else {
           request.session = session;
           request.sessionId = sidCookie;
